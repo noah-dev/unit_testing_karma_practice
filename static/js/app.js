1acea008a5b33aee6b8b874a62980b2dc938dbf6
Noah-Dev <ark_code@protonmail.com>
2017-10-30 11:51:25 -0500

    Having second thoughts about the refactoring

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,29 @@
-function Item(idx, title, desc, due){
+function SimpleItem(idx, title, desc, due){
     this.idx = idx;
     this.title = title;
     this.desc = desc;
     this.due = due;
 }
 
-function manageList(){
+function manageList(Item){
     var _this = this;
     _this.todoList = [];
     _this.addItem = addItem;
     _this.delItem = delItem;
     _this.exportList = exportList;
     _this.importList = importList;
-
-    init()
+    Item = SimpleItem;
+    //init()
     function init(){
         _this.addItem("Title 0", "Desc 0", "Due 0")
         _this.addItem("Title 1", "Desc 1", "Due 1")
         _this.addItem("Title 2", "Desc 2", "Due 2")
+    };
+
+    function add(index){
+        $http.get("").then(res=>{
+            console.log(res);
+        });
     };
 
     function addItem(title, desc, due){
@@ -43,42 +49,19 @@ function manageList(){
 }
 
 var app = angular.module("todoApp", []);
-app.controller("todoListController", [manageList]) 
+/*
+app.controller("todoListController", ['$scope', 'manageList', 'SimpleItem', function($scope, list, item){
+    console.log(list)
+    //$scope = list(SimpleItem);
+}]);
+*/
+app.controller("todoListController", [manageList]);
 
 /*
-app.controller("todoListController", function($scope) {
-    $scope.todoList = [];
-    $scope.addItem = addItem;
-    $scope.delItem = delItem;
-    $scope.exportList = exportList;
-    $scope.importList = importList;
-
-    //init()
-    function init(){
-        $scope.addItem("Title 0", "Desc 0", "Due 0")
-        $scope.addItem("Title 1", "Desc 1", "Due 1")
-        $scope.addItem("Title 2", "Desc 2", "Due 2")
-    };
-
-    function addItem(title, desc, due){
-        var idx = $scope.todoList.length;
-        var newItem = new Item(idx, title, desc, due);
-        $scope.todoList.push(newItem);
-    };
-
-    function delItem(idx){
-        $scope.todoList.splice(idx, 1);
-        for(var i = 0; i < $scope.todoList.length; i++){
-            $scope.todoList[i].idx = i;
-        }
-    };
-
-    function exportList(){
-        $scope.todoListJSON = angular.toJson($scope.todoList);
-    };
-
-    function importList(listJSONString){
-        $scope.todoList = JSON.parse(listJSONString);
-    };
+app.controller("todoListController", function($scope, $http){
+    $scope = new manageList(SimpleItem);
+    console.log($scope);
+    console.log($scope.addItem("1", "1", "1"));
+    console.log($scope);
 });
 */
